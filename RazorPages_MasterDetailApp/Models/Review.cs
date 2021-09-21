e098d28fa877e570116142da6c3de81413617d1e
dunck2020 <j.dunckel@hotmail.com>
2021-09-21 13:44:38 -0400

    Review Class added

---
+++ RazorPages_MasterDetailApp/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorPages_MasterDetailApp.Models
+{
+    public class Review
+    {
+        //Model for instantiating user comments
+        public int ReviewId;
+        public string UserName;
+        public string Comment;
+        public int Rating;
+    }
+}
