65a92c77e7725f58ee99ecc73cfcf431d94749c8
dunck2020 <j.dunckel@hotmail.com>
2021-09-23 10:30:50 -0400

    SeedDataUpdate

--- RazorPages_MasterDetailApp/Data/SeedData.cs
+++ RazorPages_MasterDetailApp/Data/SeedData.cs
@@ -19,7 +19,7 @@ namespace RazorPages_MasterDetailApp.Data
                 Author = "Becky Chambers",
                 Genres = new List<string> { "Science Fiction" },
                 PageCount = 325,
-                PubDate = "2021",
+                PubYear = 2021,
                 Description = "Return to the sprawling, Hugo Award-winning universe of the Galactic Commons to explore" +
                             "another corner of the cosmos - one often mentioned, but not yet explored - in this absorbing" +
                             "entry in the Wayfarers series, which blends heart warming characters and imaginative adventure.",
@@ -47,7 +47,6 @@ namespace RazorPages_MasterDetailApp.Data
                             UserName = "Fred"
                         }
                     },
-                AvgRating = 2
             });
             bookList.Add(new Book()
             {
@@ -56,7 +55,7 @@ namespace RazorPages_MasterDetailApp.Data
                 Author = "Jon Krakauer",
                 Genres = new List<string> { "Drama" },
                 PageCount = 207,
-                PubDate = "1996",
+                PubYear = 1996,
                 Description = "In April 1992 a young man from a well-to-do family hitchhiked to Alaska and walked alone" +
                               "into the wilderness north of Mt.McKinley.  His name was Christopher Johnson McCandlesss.  He" +
                               "had given $25,000 in savings to charity, abandoned his car and most of his possesions, burned" +
@@ -86,7 +85,6 @@ namespace RazorPages_MasterDetailApp.Data
                             UserName = "Fred"
                         }
                     },
-                AvgRating = 4.7
             });
             bookList.Add(new Book()
             {
@@ -95,7 +93,7 @@ namespace RazorPages_MasterDetailApp.Data
                 Author = "Paulo Coelho",
                 Genres = new List<string> { "Drama", "Fantasy", "Adventure" },
                 PageCount = 192,
-                PubDate = "1988",
+                PubYear = 1988,
                 Description = "This story, dazzling in its powerful simplicity and inspiring wisdom, is about an Analusian" +
                               "shepherd boy name Santiago who travels from his homeland Spain to the Eqyptian desert in search" +
                               "of treasure buried by the Pyramids.",
@@ -123,7 +121,6 @@ namespace RazorPages_MasterDetailApp.Data
                             UserName = "Booklist"
                         }
                     },
-                AvgRating = 2
             });
             return bookList;
         }
