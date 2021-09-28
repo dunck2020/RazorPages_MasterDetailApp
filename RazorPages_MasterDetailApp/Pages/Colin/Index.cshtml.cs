f55ef4c196b52e2945b1ac62fc2ff9d623a31ce2
coharou <60243200+coharou@users.noreply.github.com>
2021-09-27 21:20:02 -0400

    Completed main and detail pages

--- RazorPages_MasterDetailApp/Pages/Colin/Index.cshtml.cs
+++ RazorPages_MasterDetailApp/Pages/Colin/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorPages_MasterDetailApp.Data;
 using RazorPages_MasterDetailApp.Models;
@@ -21,9 +22,54 @@ namespace RazorPages_MasterDetailApp.Pages.Colin
 
         public IList<Book> Book { get;set; }
 
-        public async Task OnGetAsync()
+        [BindProperty(SupportsGet = true)]
+        public string TitleSearch { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string AuthorSearch { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string GenreSearch { get; set; }
+
+        public string TypeOfSort { get; set; }
+
+        public async Task OnGetAsync(string sortType)
         {
-            Book = await _context.Book.ToListAsync();
+            var books = from b in _context.Book
+                        select b;
+
+            TypeOfSort = sortType;
+
+            if (!string.IsNullOrEmpty(TitleSearch))
+            {
+                books = books.Where(s => s.Title.Contains(TitleSearch));
+            }
+            if (!string.IsNullOrEmpty(AuthorSearch))
+            {
+                books = books.Where(s => s.Author.Contains(AuthorSearch));
+            }
+            if (!string.IsNullOrEmpty(GenreSearch))
+            {
+                books = books.Where(s => s.Genres.Contains(GenreSearch));
+            }
+
+            if (!string.IsNullOrEmpty(TypeOfSort))
+            {
+                if (TypeOfSort == "title")
+                {
+                    books = books.OrderBy(b => b.Title);
+                }
+                if (TypeOfSort == "pubyear")
+                {
+                    books = books.OrderBy(b => b.PubYear);
+                }
+                if (TypeOfSort == "pagecount")
+                {
+                    books = books.OrderBy(b => b.PageCount);
+                }
+            }
+
+            Book = await books.ToListAsync();
         }
     }
 }
