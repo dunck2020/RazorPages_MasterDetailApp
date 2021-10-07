ddb1c8ee2b86ec182b6b4475b904cc684267d3f6
dunck2020 <j.dunckel@hotmail.com>
2021-10-07 15:00:32 -0400

    Style Updates

---
+++ RazorPages_MasterDetailApp/Migrations/20211005184936_modelUpdate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RazorPages_MasterDetailApp.Migrations
+{
+    public partial class modelUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
